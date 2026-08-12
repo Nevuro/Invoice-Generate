11648d22dbedf8ba9fd40fedf4f0fa2eafdcd835
Nevuro <119460199+Nevuro@users.noreply.github.com>
2026-08-12 18:12:03 +0300

    update

--- app.py
+++ app.py
@@ -197,4 +197,4 @@ def delete_invoice(inv_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5090, debug=True, threaded=True)
